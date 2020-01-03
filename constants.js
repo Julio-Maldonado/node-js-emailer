d3a21bdc41f0f0b9c52d949ba52376244ea8b684
Julio-Maldonado <juliom@Julios-MacBook-Pro-3.local>
2020-01-02 23:05:23 -0600

    handle yeux requests

--- constants.js
+++ constants.js
@@ -1,11 +1,13 @@
-const key1 = '', key2 = '', your_name = '', your_email = ''
+const your_name = 'Julio Maldonado', your_email = 'julioharlingen@gmail.com'
+
+const key1 = 'c5fc907dcda19f8cefb3be106edfc67f', key2 = 'c179b4d9b4ee638401c04d18fd60a789'
 
 const mailjet = require('node-mailjet').connect(key1, key2)
 
 module.exports = {
     DEFAULT_MESSAGE: '<h3>Howdy!</h3> <br/> <br/>'
     + "Thanks for signing up for my newsletter through <a href='https://www.juliomaldonado.com/'>juliomaldonado.com</a> :)<br/><br/>"
-    + '<img src="https://scontent-sea1-1.cdninstagram.com/vp/c04ec4e6cf1f88bd6e195569af4990a9/5DEFAB3F/t51.2885-15/e35/36940224_205770806781401_4820515398602457088_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com" alt="Image of overview of Provo, Utah" height="100%" width="100%" /><br/><br/>'
+    + '<img src="https://scontent-dfw5-1.cdninstagram.com/v/t51.2885-15/e35/36940224_205770806781401_4820515398602457088_n.jpg?_nc_ht=scontent-dfw5-1.cdninstagram.com&_nc_cat=105&_nc_ohc=8SSJFNKzWHoAX-sb32-&oh=27d22134ec5c73a61c7d685ddf387e78&oe=5E8DDF3F" alt="Image of overview of Provo, Utah" height="100%" width="100%" /><br/><br/>'
     + "I'll share life experiences, lessons, big news, and more as they come.<br/><br/>"
     + 'Ciao!<br/><br/>'
     + '<i>Julio Maldonado</i><br/><br/><br/>'
@@ -30,6 +32,40 @@ module.exports = {
               HTMLPart: message,
             },
           ],
+        }).then(result => {
+            console.log(result);
+            return true;
+        }).catch(err => {
+            console.log(err);
+            return false;
         })
+    ),
+    YEUX_MESSAGE: '',
+    yeux_request: (name, emailAddress, subject, message) => (
+      mailjet.post('send', { version: 'v3.1' }).request({
+        Messages: [
+          {
+            From: {
+              Email: emailAddress,
+              Name: name,
+            },
+            To: [
+              {
+                Email: 'julio.maldonado.guzman@gmail.com',
+                Name: "Yeux",
+              }
+            ],
+            Subject: subject,
+            TextPart: "Yeux Website Inquiry",
+            HTMLPart: message,
+          }
+        ]
+      }).then(result => {
+        console.log(result);
+        return true;
+      }).catch(err => {
+        console.log(err);
+        return false;
+      })
     ),
 }
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const cors = require('cors');
 const express = require("express");
 const bodyParser = require("body-parser");
 const CONSTANTS = require("./constants");
@@ -8,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,7 +25,8 @@ app.use(function(req, res, next) {
   const allowedOrigins = [
     "http://127.0.0.1:7999",
     "http://localhost:7999",
-    "juliomaldonado.com"
+    "juliomaldonado.com",
+    "yeux.tech"
   ];
   const origin = req.headers.origin;
   if (allowedOrigins.indexOf(origin) > -1) {
@@ -38,26 +42,54 @@ app.use(function(req, res, next) {
 
 app.post("/api/send_email", (req, res) => {
   console.log("STARTING API/SEND_EMAIL ACTION");
+  if ("emailAddress" in req.body) {
+    const emailAddress = req.body.emailAddress;
+    const name = req.body.name;
+    const subject = req.body.subject;
+    const message = req.body.message;
 
-  let email = req.body.email;
-  CONSTANTS.request("juliom72@tamu.edu", email, "New user Sign Up")
-    .then(() => {
-      console.log("email sent to juliom72@tamu.edu");
-    })
-    .then(
-      CONSTANTS.request(
-        email,
-        CONSTANTS.DEFAULT_MESSAGE,
-        CONSTANTS.DEFAULT_SUBJECT
+    CONSTANTS.yeux_request(name, emailAddress, subject, message)
+      .then(() => {
+        console.log("email sent to", emailAddress);
+      })
+      .then(
+        CONSTANTS.yeux_request(
+          "Yeux",
+          "julio.maldonado.guzman@gmail.com",
+          "Yeux Email confirmation",
+          "Your email has been received by Yeux. We will respond promptly. Have a great day!"
+        )
       )
-    )
-    .then(result => {
-      res.send({ success: true, result });
-    })
-    .catch(err => {
-      console.log(err);
-      res.send({ succes: false, statusCode: err.statusCode });
-    });
+      .then(result => {
+        console.log(result);
+        res.send({ success: true, result });
+      })
+      .catch(err => {
+        console.log(err);
+        res.send({ succes: false, statusCode: err.statusCode });
+      });
+  } else {
+    const email = req.body.email;
+    CONSTANTS.request("juliom72@tamu.edu", email, "New user Sign Up")
+      .then(() => {
+        console.log("email sent to juliom72@tamu.edu");
+      })
+      .then(
+        CONSTANTS.request(
+          email,
+          CONSTANTS.DEFAULT_MESSAGE,
+          CONSTANTS.DEFAULT_SUBJECT
+        )
+      )
+      .then(result => {
+        console.log(result);
+        res.send({ success: true, result });
+      })
+      .catch(err => {
+        console.log(err);
+        res.send({ succes: false, statusCode: err.statusCode });
+      });
+  }
 });
 
 app.get("/", (req, res) => res.render("pages/index"));
